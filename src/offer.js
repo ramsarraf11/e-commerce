3aeec8f3140df89f2596f09399b9d2fc1c3a9b22
Shyam Sarraf <ramsarraf00@gmail.com>
2023-03-09 16:48:14 +0530

    work done

---
+++ src/offer.js
@@ -0,0 +1,28 @@
+let cartItem = JSON.parse(localStorage.getItem("cart"));
+displayCard(cartItem);
+let totalCost = cartItem.reduce((acc, el) => acc + el.price, 0);
+document.querySelector("#cart-total").innerText = totalCost;
+
+function search() {
+    let q = document.querySelector("select").value;
+    if (q == 2) {
+        document.querySelector("#cart-total").innerText
+    }
+}
+function displayCard(product) {
+    product.forEach((elem) => {
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        img.setAttribute("src", elem.img)
+        let SN = document.createElement("p")
+        SN.innerText = elem.id;
+        let brand = document.createElement("p");
+        brand.innerText = "Brand:  " + elem.brand;
+        let price = document.createElement("p");
+        price.innerText = "price:  " + elem.price;
+        let category = document.createElement("p");
+        category.innerText = "category:  " + elem.category;
+        div.append(img, SN, brand, price, category);
+        document.querySelector("#cart-container").append(div);
+    });
+}
